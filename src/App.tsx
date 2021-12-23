a57bdd6e8d4d8a26e8e83922718c7f497daec561
LuciferWZX <2396423791@qq.com>
2021-12-23 21:48:07 +0800

    1.新增ESLint 2.新增Prettier 3.新增Husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-
-import '@pages/home'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import React, { FC } from "react";
+import "@pages/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "@pages/home";
 import Layouts from "@layouts/index";
 import NoMatch from "@pages/noMatch";
 
-function App() {
+const App: FC = () => {
   return (
-      <Router>
-        <Routes>
-          <Route path={'/'} element={<Layouts/>} >
-              <Route index element={<Home/>} />
-              <Route
-                  path="*"
-                  element={<NoMatch/>}
-              />
-          </Route>
-        </Routes>
-      </Router>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path={"/"} element={<Layouts />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
